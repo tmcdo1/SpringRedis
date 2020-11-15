f1acfb0e6cdabbe1915011e6d936d7771bc062e8
Thomas McDonald <thomas.carnegie.1234@gmail.com>
2020-11-14 18:28:58 -0600

    added h2 config and db repositories

---
+++ src/main/java/com/credera/SpringRedis/repositories/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.credera.SpringRedis.repositories;
+
+import com.credera.SpringRedis.entities.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
+}
---
+++ src/main/java/com/credera/SpringRedis/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.credera.SpringRedis.repositories;
+
+import com.credera.SpringRedis.entities.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<OrderEntity, Long> {
+}
