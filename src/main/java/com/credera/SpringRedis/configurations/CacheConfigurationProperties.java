fe990912cc5c913f0ebc174853d4a4bc085add14
Thomas McDonald <thomas.carnegie.1234@gmail.com>
2020-11-15 14:40:44 -0600

    expanded on redis configuration

---
+++ src/main/java/com/credera/SpringRedis/configurations/CacheConfigurationProperties.java
@@ -0,0 +1,24 @@
+package com.credera.SpringRedis.configurations;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ConfigurationProperties(prefix = "spring.redis")
+@Data
+public class CacheConfigurationProperties {
+
+    private String redisHost;
+
+    private String redisPass;
+
+    private int redisPort;
+
+    private long timeoutSeconds = 60;
+
+    // Mapping of cacheNames to expiration-after-write timeout in seconds
+    private Map<String, Long> cacheExpirations = new HashMap<>();
+}
+
--- src/main/java/com/credera/SpringRedis/configurations/RedisConfiguration.java
+++ src/main/java/com/credera/SpringRedis/configurations/RedisConfiguration.java
@@ -1,36 +1,61 @@
 package com.credera.SpringRedis.configurations;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class RedisConfiguration {
 
-    @Value("${spring.redis.host}")
-    private String redisHost;
-
-    @Value("${spring.redis.password}")
-    private String redisPass;
-
-    @Value("${spring.redis.port}")
-    private int redisPort;
+    private static RedisCacheConfiguration createCacheConfiguration(long timeoutInSeconds) {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(timeoutInSeconds));
+    }
 
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
-        redisStandaloneConfiguration.setPassword(RedisPassword.of(redisPass));
+    JedisConnectionFactory jedisConnectionFactory(CacheConfigurationProperties properties) {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(properties.getRedisHost(), properties.getRedisPort());
+        if (properties.getRedisPass() != null && properties.getRedisPass().length() > 0) {
+            redisStandaloneConfiguration.setPassword(RedisPassword.of(properties.getRedisPass()));
+        }
         return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
-    public RedisTemplate redisTemplate() {
+    public RedisTemplate redisTemplate(CacheConfigurationProperties properties) {
         RedisTemplate template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
+        template.setConnectionFactory(jedisConnectionFactory(properties));
         return template;
+    }
+
+    @Bean
+    public RedisCacheConfiguration cacheConfiguration(CacheConfigurationProperties properties) {
+        return createCacheConfiguration(properties.getTimeoutSeconds());
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory, CacheConfigurationProperties properties) {
+        Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
+
+        for (Map.Entry<String, Long> cacheNameAndTimeout : properties.getCacheExpirations().entrySet()) {
+            cacheConfigurations.put(cacheNameAndTimeout.getKey(), createCacheConfiguration(cacheNameAndTimeout.getValue()));
+        }
+
+        return RedisCacheManager
+                .builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfiguration(properties))
+                .withInitialCacheConfigurations(cacheConfigurations).build();
     }
 }
